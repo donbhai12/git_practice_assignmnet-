8c62808bbfc70cae08b693e6cf4dd47c7d868ae9
animesh-0041 <ghoroianimesh1508@gmail.com>
2022-11-02 11:58:02 +0530

    Prime code has been fixed

--- prime.js
+++ prime.js
@@ -1,12 +1,20 @@
-function str(prime){
-let count=0;
-for(let i=0;i<=prime;i++){
-  if (prime%2==0){
-   count++;
-}
-}
-if(count==2){
- 
+function Isprimer(prime) {
+  let count = 0;
+  for (let i = 1; i <= prime; i++) {
+    if (prime % 2 == 0) {
+      count++;
+    }
+  }
+  if (count == 2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+let result = Isprimer(13);
+if (result == true) {
+  console.log("Prime");
+} else {
+  console.log("Not Prime");
 }
